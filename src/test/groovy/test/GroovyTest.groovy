cbbc4892d6ed94013646f39fc6d9a393115dddc4
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-08-23 17:38:18 +0200

    Initial commit

---
+++ src/test/groovy/test/GroovyTest.groovy
@@ -0,0 +1,14 @@
+package test
+
+import spock.lang.Specification
+
+/**
+ * Created by mtumilowicz on 2018-08-23.
+ */
+class GroovyTest extends Specification {
+    def "test"() {
+        println 'GroovyTest'
+        expect:
+        true
+    }
+}
---
+++ src/test/java/integration/IntegrationIT.java
@@ -0,0 +1,16 @@
+package integration;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by mtumilowicz on 2018-08-23.
+ */
+public class IntegrationIT {
+    @Test
+    public void test() {
+        System.out.println("IntegrationIT");
+        assertTrue(true);
+    }
+}
